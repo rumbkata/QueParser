39ceac0a3a481b38e91c00ae8427df6d0cd83365
Rumen Jordakiev <jordakievr@gmail.com>
2022-09-22 22:53:19 +0300

    Complete parseI(...) function

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -82,5 +82,17 @@ static TreeNode *Parser::parseR(const std::string &str, int &index, TreeNode *&l
 
 static void Parser::parseI(const std::string &str, int &index)
 {
-    skipSpaces(str, index);
+    std::string result(str.at(index++));
+    while (isDigit(str.at(index)))
+    {
+        result.append(str.at(index++), 0, 1);
+    }
+    // calculate the number
+    int number = 0;
+    int power = 0;
+    for (size_t i = result.length - 1; i >= 0; i--)
+    {
+        number += result.at(i) * pow(10, power++);
+    }
+    return new Number(number);
 }
